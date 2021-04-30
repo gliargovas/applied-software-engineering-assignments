e04b4f621920ef7ebeef609cee030c7e55185a9c
George Liargkovas <t8180061@aueb.gr>
2021-04-30 03:37:06 +0300

    Add ArrayOperations class
    ArrayOperations contains findPrimesInFile method used to generate an array with the prime numbers read from a file.

---
+++ unittesting/src/main/java/math/ArrayOperations.java
@@ -0,0 +1,43 @@
+package math;
+
+import io.FileIO;
+
+import java.util.ArrayList;
+
+/**
+ * The ArrayOperations provides simple array operations
+ * that serve as hands-on practice on Unit Testing.
+ *
+ * @author gliargovas
+ * @version 1.0
+ * @since 2021-04-29
+ */
+
+public class ArrayOperations {
+
+    /**
+     * Performs the basic arithmetic operation of division.
+     *
+     * @param fileIo   a FileIO object
+     * @param filepath the path of the file of which to read the numbers
+     * @param myMath   a MyMath object
+     * @return the result of the division between numerator and denominator
+     * @throws IllegalArgumentException when a number less than 2 exists in the array
+     */
+    public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
+        int[] nums = fileIo.readFileModified(filepath);
+        ArrayList<Integer> prime_nums = new ArrayList<Integer>();
+        for (int num : nums) {
+            if (myMath.isPrime(num)) {
+                prime_nums.add(num);
+            }
+        }
+        int[] primeNumArray = new int[prime_nums.size()];
+        int i = 0;
+        for (Integer num : prime_nums) {
+            primeNumArray[i] = num;
+            i++;
+        }
+        return primeNumArray;
+    }
+}
