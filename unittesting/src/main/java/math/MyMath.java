7a0c287e1ca8d0d5d537180fd2448190d4eafcc4
George Liargkovas <t8180061@aueb.gr>
2021-04-30 03:31:43 +0300

    Add isPrime method and improve documentation
    * Add a more descriptive documentation to factorial method
    * Add isPrime method that returns whether a given integer is prime or not

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -10,7 +10,7 @@ package math;
 public class MyMath {
 
     /**
-     * Factorial int.
+     * A method for calculating the factorial of an integer between 0 and 12 recursively.
      *
      * @param n the number of which to calculate the factorial
      * @return the factorial of n
@@ -23,6 +23,30 @@ public class MyMath {
             return 1;
         } else {
             return n * factorial(n - 1);
+        }
+    }
+
+    /**
+     * A method for checking whether an integer is prime or not.
+     *
+     * @param n to check
+     * @return whether n is prime or not
+     * @throws IllegalArgumentException when <b>n</b> less than 2.
+     */
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("n should be larger than 2");
+        } else {
+            int i = 2;
+            boolean prime = true;
+            while (i <= n / 2) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+                ++i;
+            }
+            return prime;
         }
     }
 }
