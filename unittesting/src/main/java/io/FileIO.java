3d4d31aa98c73a7af96f1398414837691fc28c12
gliargovas <t8180061@aueb.gr>
2021-05-11 12:39:26 +0300

    Remove redundant method
    This commit addresses issue #8 by removing the redundant readFileModified method.

--- unittesting/src/main/java/io/FileIO.java
+++ unittesting/src/main/java/io/FileIO.java
@@ -18,41 +18,6 @@ import java.util.List;
  */
 public class FileIO {
 
-    /**
-     * Reads a file that contains numbers line by line
-     * and returns an array of the integers found in the file.
-     *
-     * @param filepath the file that contains the numbers
-     * @return an array of numbers
-     * @throws IllegalArgumentException when the given file does not exist
-     * @throws IllegalArgumentException when the given file is empty
-     * @throws IOException              when an IO interruption occurs (not required to be tested)
-     */
-    public int[] readFile(String filepath) {
-        File file = new File(filepath);
-        if (!file.exists())
-            throw new IllegalArgumentException("Input file does not exist");
-
-        List<Integer> numbersList = new ArrayList<>();
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                int number = Integer.parseInt(line);
-                numbersList.add(number);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (numbersList.size() == 0)
-            throw new IllegalArgumentException("Given file is empty");
-
-        // Convert a List to an array using
-        return numbersList.stream().mapToInt(i -> i).toArray();
-    }
-
 
     /**
      * Reads a file that contains numbers line by line
@@ -65,7 +30,7 @@ public class FileIO {
      * @throws IllegalArgumentException when the given file is empty
      * @throws IOException              when an IO interruption occurs (not required to be tested)
      */
-    public int[] readFileModified(String filepath) {
+    public int[] readFile(String filepath) {
         File file = new File(filepath);
         if (!file.exists())
             throw new IllegalArgumentException("Input file does not exist");
